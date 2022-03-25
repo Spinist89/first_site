32d326c45a85051883157f34019292ef8251a86e
alex <spinist.89@mail.ru>
2022-03-25 22:56:40 +0300

    add gallery

---
+++ static/js/main.js
@@ -0,0 +1,8 @@
+<!-- jQuery -->
+<script src="путь_до/jquery-3.2.1.min.js"></script>
+<!-- Popper -->
+<script src="путь_до/popper.min.js"></script>
+<!-- Bootstrap JS -->
+<script src="путь_до/bootstrap.min.js"></script>
+<!-- fancyBox -->
+<script src="путь_до/jquery.fancybox.min.js"></script>
