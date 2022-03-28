fe07a0cf22de03abf7d1173c09d9e939610f2feb
alex <spinist.89@mail.ru>
2022-03-28 23:19:09 +0300

    minor

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
 
 @app.route('/masters')
 def masters():
